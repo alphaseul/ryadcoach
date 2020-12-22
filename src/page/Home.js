91e9b45fa1862cd4194e0429a367b328f721c297
alphaseul <alphaseul@yahoo.fr>
2020-12-22 16:30:08 +0100

    caroussel

--- src/page/Home.js
+++ src/page/Home.js
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import "./css/home.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Carousel } from "react-bootstrap";
+import img1 from "./img/img-1.jpg";
+import img2 from "./img/img-2.jpeg";
 
 function Home() {
   useEffect(() => {
@@ -13,7 +16,7 @@ function Home() {
   return (
     <body>
       <section id="hero" class="d-flex align-items-center">
-        <div class="container" data-aos="zoom-out" data-aos-delay="100">
+        <div class="container " data-aos="zoom-out" data-aos-delay="100">
           <div class="row">
             <div class="col-xl-6">
               <h1>Coach en ligne</h1>
@@ -104,94 +107,40 @@ function Home() {
 
         <section id="tabs" class="tabs">
           <div class="container" data-aos="fade-up">
-            <ul class="nav nav-tabs row d-flex">
-              <li class="nav-item col-3">
-                <a
-                  class="nav-link active show"
-                  data-bs-toggle="tab"
-                  data-bs-target="#tab-1"
-                >
-                  <i class="ri-gps-line"></i>
-                  <h4 class="d-none d-lg-block">
-                    Modi sit est dela pireda nest
-                  </h4>
-                </a>
-              </li>
-              <li class="nav-item col-3">
-                <a
-                  class="nav-link"
-                  data-bs-toggle="tab"
-                  data-bs-target="#tab-2"
-                >
-                  <i class="ri-body-scan-line"></i>
-                  <h4 class="d-none d-lg-block">
-                    Unde praesenti mara setra le
-                  </h4>
-                </a>
-              </li>
-              <li class="nav-item col-3">
-                <a
-                  class="nav-link"
-                  data-bs-toggle="tab"
-                  data-bs-target="#tab-3"
-                >
-                  <i class="ri-sun-line"></i>
-                  <h4 class="d-none d-lg-block">Pariatur explica nitro dela</h4>
-                </a>
-              </li>
-              <li class="nav-item col-3">
-                <a
-                  class="nav-link"
-                  data-bs-toggle="tab"
-                  data-bs-target="#tab-4"
-                >
-                  <i class="ri-store-line"></i>
-                  <h4 class="d-none d-lg-block">Nostrum qui dile node</h4>
-                </a>
-              </li>
-            </ul>
-
             <div class="tab-content">
               <div class="tab-pane active show" id="tab-1">
                 <div class="row">
-                  <div
-                    class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0"
-                    data-aos="fade-up"
-                    data-aos-delay="100"
-                  >
-                    <h3>
-                      Voluptatem dignissimos provident quasi corporis voluptates
-                      sit assumenda.
-                    </h3>
-                    <p class="font-italic">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua.
-                    </p>
-                    <ul>
-                      <li>
-                        <i class="ri-check-double-line"></i> Ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat.
-                      </li>
-                      <li>
-                        <i class="ri-check-double-line"></i> Duis aute irure
-                        dolor in reprehenderit in voluptate velit.
-                      </li>
-                      <li>
-                        <i class="ri-check-double-line"></i> Ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate trideta
-                        storacalaperda mastiro dolore eu fugiat nulla pariatur.
-                      </li>
-                    </ul>
-                    <p>
-                      Ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                      Duis aute irure dolor in reprehenderit in voluptate velit
-                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                      sint occaecat cupidatat non proident, sunt in culpa qui
-                      officia deserunt mollit anim id est laborum
-                    </p>
-                  </div>
+                  <Carousel>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src={img1}
+                        alt="First slide"
+                      />
+                      <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>
+                          Nulla vitae elit libero, a pharetra augue mollis
+                          interdum.
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100"
+                        src={img2}
+                        alt="Third slide"
+                      />
+
+                      <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit.
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                  </Carousel>
                   <div
                     class="col-lg-6 order-1 order-lg-2 text-center"
                     data-aos="fade-up"
