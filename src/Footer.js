da26020b7c6b68f7dc7c0fbfb00c272f9f82be6b
alphaseul <alphaseul@yahoo.fr>
2020-12-22 14:59:31 +0100

    design commit

---
+++ src/Footer.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <div class="footer-top">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-3 col-md-6 footer-contact">
+              <h3>
+                Presento<span>.</span>
+              </h3>
+              <p>
+                A108 Adam Street <br />
+                New York, NY 535022
+                <br />
+                United States <br />
+                <br />
+                <strong>Phone:</strong> +1 5589 55488 55
+                <br />
+                <strong>Email:</strong> info@example.com
+                <br />
+              </p>
+            </div>
+
+            <div class="col-lg-2 col-md-6 footer-links">
+              <h4>Liens Utiles</h4>
+              <ul>
+                <li>
+                  <i class="bx bx-chevron-right"></i> <a href="#">Accueil</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i> <a href="#">A propos</a>
+                </li>
+                <li>
+                  <i class="bx bx-chevron-right"></i> <a href="#">Services</a>
+                </li>
+
+                <li>
+                  <i class="bx bx-chevron-right"></i>{" "}
+                  <a href="#">Privacy policy</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-lg-4 col-md-6 footer-newsletter">
+              <h4>Join Our Newsletter</h4>
+              <p>
+                Tamen quem nulla quae legam multos aute sint culpa legam noster
+                magna
+              </p>
+              <form action="" method="post">
+                <input type="email" name="email" />
+                <input type="submit" value="Subscribe" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container d-md-flex py-4">
+        <div class="me-md-auto text-center text-md-left">
+          <div class="copyright">
+            &copy; Copyright{" "}
+            <strong>
+              <span>qrcode-protect</span>
+            </strong>
+            . All Rights Reserved
+          </div>
+        </div>
+        <div class="social-links text-center text-md-right pt-3 pt-md-0"></div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Navbar.css";
+import { CgProfile } from "react-icons/cg";
+
+function Navbar() {
+  return (
+    <header id="header" className="">
+      <div className="container d-flex align-items-center">
+        <h1 className="logo me-auto">
+          <a href="index.html">
+            Ryad Coach Sportif<span>.</span>
+          </a>
+        </h1>
+
+        <a href="index.html" className="logo me-auto">
+          <img src="assets/img/logo.png" alt="" />
+        </a>
+
+        <nav className="nav-menu d-none d-lg-block">
+          <ul>
+            <li className="active">
+              <a href="#header">Acceuil</a>
+            </li>
+            <li>
+              <a href="#about">A propos</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#pricing">Abonnements</a>
+            </li>
+            <li>
+              <a href="blog.html">Blog</a>
+            </li>
+
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li className="drop-down">
+              <a href="/#">
+                <CgProfile />
+              </a>
+              <ul>
+                <li>
+                  <a href="/#">Inscription</a>
+                </li>
+                <li>
+                  <a href="/#">Connexion</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
