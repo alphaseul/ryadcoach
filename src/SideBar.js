53015041efdcccdaa40ee3eb1ee00c184661b27c
alphaseul <alphaseul@yahoo.fr>
2020-12-31 12:01:55 +0100

    side bar responsive

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,28 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sidebar.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Dashboard from "./page/Dashboard";
 import Seances from "./components/Seances.component";
 import img from "./profil.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SideBar = (props) => {
+  const [StyleNav, setStyleNav] = useState({});
+  function openNav() {
+    setStyleNav({
+      marginLeft: "0px",
+    });
+  }
+  function closeNav() {
+    setStyleNav({
+      marginLeft: "-1000px",
+    });
+  }
+
   return (
     <div>
       {props.currentUser && (
         <Router>
-          <div className="headerSide">
+          <div className="toggle-button d-flex">
+            <span onClick={openNav}>
+              <GiHamburgerMenu size="45px" />
+            </span>
+          </div>
+          <div className="headerSide" style={StyleNav}>
             <div className="d-flex flex-column">
               <div className="profile">
-                <img
-                  src={
-                    props.currentUser.photo_profil
-                      ? "http://82.165.184.180:1337" +
-                        props.currentUser.photo_profil.url
-                      : img
-                  }
-                  alt=""
-                  className="img-fluid rounded-circle"
-                />
+                <div className="d-flex row">
+                  <img
+                    src={
+                      props.currentUser.photo_profil
+                        ? "http://82.165.184.180:1337" +
+                          props.currentUser.photo_profil.url
+                        : img
+                    }
+                    alt=""
+                    className="img-fluid rounded-circle"
+                  />
+                  <span onClick={closeNav} className="toggle-close">
+                    <AiOutlineClose size="30px" />
+                  </span>
+                </div>
+
                 <h1 className="text-light">
                   <a href="/">
                     <b>{props.currentUser.username}</b>
@@ -33,10 +58,12 @@ const SideBar = (props) => {
               <nav className="nav-menu-side">
                 <ul>
                   <li>
-                    <Link to="/mon_profil">Mon Profil</Link>
+                    <Link to="/mon_profil" onClick={closeNav}>
+                      Mon Profil
+                    </Link>
                   </li>
                   <li>
-                    <Link to="/Entrainements">
+                    <Link to="/Entrainements" onClick={closeNav}>
                       <i className="bx"></i> <span>Entrainements</span>
                     </Link>
                   </li>
@@ -57,7 +84,7 @@ const SideBar = (props) => {
                 </ul>
               </nav>
             </div>
-            <div className="footer-side" id="footer">
+            <div className="footer-side">
               <div className="container">
                 <div className="copyright">
                   &copy; Copyright{" "}
--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -7,92 +7,89 @@ const Dashboard = (props) => {
   console.log(user);
 
   return (
-    <div style={{ backgroundColor: "#e6e6e6", height: "100%" }}>
-      <section id="about" className="about conntainer">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 d-flex  column align-items-center">
-              <img
-                src={
-                  user.photo_profil
-                    ? "http://82.165.184.180:1337" + user.photo_profil.url
-                    : img
-                }
-                className="img-fluid"
-                alt=""
-                width="250px"
-                style={{ marginLeft: "30%" }}
-              />
-            </div>
-            <div className="col-lg-8 pt-4 pt-lg-0 content">
-              <h3>Mes Informations</h3>
+    <section id="about" className="about ">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 d-flex  column justify-content-center">
+            <img
+              src={
+                user.photo_profil
+                  ? "http://82.165.184.180:1337" + user.photo_profil.url
+                  : img
+              }
+              className="img-fluid"
+              alt=""
+              width="250px"
+            />
+          </div>
+          <div className="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>Mes Informations</h3>
 
-              <div className="row">
-                <div className="col-lg-6">
-                  <ul>
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>date de naissance:</strong> XX XX XXXX
-                    </li>
+            <div className="row">
+              <div className="col-lg-6">
+                <ul>
+                  <li>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>date de naissance:</strong> XX XX XXXX
+                  </li>
 
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>Tel:</strong> +33 XXXXXXXXX
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-lg-6">
-                  <ul>
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>Age:</strong> XX
-                    </li>
-
-                    <li>
-                      <i className="icofont-rounded-right"></i>{" "}
-                      <strong>Email:</strong> {user.email}
-                    </li>
-                  </ul>
-                </div>
+                  <li>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>Tel:</strong> +33 XXXXXXXXX
+                  </li>
+                </ul>
               </div>
-              <div className="mt-3">
-                <h3>Mes Statistiques</h3>
-                <div className="container">
-                  <div className="row">
-                    <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-                      <div className="count-box">
-                        <span>232</span>
-                        <p>
-                          <strong>XXXXXXXXX</strong>
-                        </p>
-                      </div>
-                    </div>
+              <div className="col-lg-6">
+                <ul>
+                  <li>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>Age:</strong> XX
+                  </li>
 
-                    <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-                      <div className="count-box">
-                        <span>521</span>
-                        <p>
-                          <strong>XXXXXXXXX</strong>
-                        </p>
-                      </div>
+                  <li>
+                    <i className="icofont-rounded-right"></i>{" "}
+                    <strong>Email:</strong> {user.email}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="mt-3">
+              <h3>Mes Statistiques</h3>
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <span>232</span>
+                      <p>
+                        <strong>XXXXXXXXX</strong>
+                      </p>
                     </div>
+                  </div>
 
-                    <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-                      <div className="count-box">
-                        <span>1,463</span>
-                        <p>
-                          <strong>XXXXXXXXX</strong>
-                        </p>
-                      </div>
+                  <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <span>521</span>
+                      <p>
+                        <strong>XXXXXXXXX</strong>
+                      </p>
                     </div>
+                  </div>
 
-                    <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
-                      <div className="count-box">
-                        <span>15</span>
-                        <p>
-                          <strong>XXXXXXXXX</strong>
-                        </p>
-                      </div>
+                  <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <span>1,463</span>
+                      <p>
+                        <strong>XXXXXXXXX</strong>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-3 col-md-6 d-md-flex align-items-md-stretch">
+                    <div className="count-box">
+                      <span>15</span>
+                      <p>
+                        <strong>XXXXXXXXX</strong>
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -100,8 +97,8 @@ const Dashboard = (props) => {
             </div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
